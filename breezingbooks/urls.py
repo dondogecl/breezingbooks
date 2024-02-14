17692325fdbfc61325cf5c0008e561836ac0874e
Alejandro Acevedo <ale@doges.dev>
2024-02-14 00:20:05 -0500

    initiallized basic Django config, created a home template and basic search template for testing urls

--- breezingbooks/urls.py
+++ breezingbooks/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import reviews.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', reviews.views.index, name='index'),
+    path('search/', reviews.views.search, name='search'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # This is the view for the index page of the reviews app.
+    name = request.GET.get("name") or "nobody"
+    return render(request, "base.html", {"name": name})
+
+def search(request):
+    # this is the view for the search page of the reviews app.
+    search_term = request.GET.get("q")
+    return render(request, "search.html", {"search_text": search_term})
